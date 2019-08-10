3df2293373124b8d0a08fefa10ff6451e3d4dd97
David Crocker <mail@djcrocker.com>
2019-08-10 12:07:58 -0400

    Incomplete attempt at GPU accelerated simulation

--- script.js
+++ script.js
@@ -64,6 +64,7 @@ function init() {
 
   initShaders();
   initBuffers();
+  initTextures();
 
   window.addEventListener('resize', resizeCanvas);
   resizeCanvas();
@@ -146,6 +147,41 @@ function initShaders() {
     shaderProgram,
     'u_PointSize'
   );
+}
+
+const Texture = function() {
+  const tex = (this.texture = gl.createTexture());
+  gl.bindTexture(gl.TEXTURE_2D, tex);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+};
+
+Texture.prototype.bind = function() {
+  gl.bindTexture(gl.TEXTURE_2D, this.texture);
+};
+
+Texture.prototype.blank = function(w, h) {
+  this.bind();
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    w,
+    h,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    null
+  );
+};
+
+function initTextures() {
+  const pos1 = new Texture();
+  const pos2 = new Texture();
+  const vel1 = new Texture();
+  const vel2 = new Texture();
 }
 
 function initBuffers() {
